05526b42695e08495a4decb4e058241f893401cb
Rushali Asthana <rushaliasthana@gmail.com>
2020-08-10 19:24:42 +0530

    Updates

---
+++ precache-manifest.57f81e61fe70d543b16e8e64ea2a7851.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ef91765c8da2ce285cb08c6200c58b",
+    "url": "/MeditationTherapy/index.html"
+  },
+  {
+    "revision": "58f6aa8485ff3ed2ea6c",
+    "url": "/MeditationTherapy/static/css/main.1f8663ef.chunk.css"
+  },
+  {
+    "revision": "29a2bbe891d400c3bcf5",
+    "url": "/MeditationTherapy/static/js/2.d9082ffa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MeditationTherapy/static/js/2.d9082ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f6aa8485ff3ed2ea6c",
+    "url": "/MeditationTherapy/static/js/main.8aa29260.chunk.js"
+  },
+  {
+    "revision": "2a594b6eb536fffbabac",
+    "url": "/MeditationTherapy/static/js/runtime-main.1cfae9df.js"
+  },
+  {
+    "revision": "54ec112620877f092f286ec9dcf9854f",
+    "url": "/MeditationTherapy/static/media/Bg.54ec1126.png"
+  },
+  {
+    "revision": "79477dd39967a68c39be8affe0d6f939",
+    "url": "/MeditationTherapy/static/media/Group 1.79477dd3.png"
+  },
+  {
+    "revision": "c368c6f125335408de946287c500be8f",
+    "url": "/MeditationTherapy/static/media/Group 2.c368c6f1.png"
+  },
+  {
+    "revision": "8db3853713d7a335d36b3f22fab08902",
+    "url": "/MeditationTherapy/static/media/Group 3.8db38537.png"
+  },
+  {
+    "revision": "aa6d1317cf3d9bb1ac15ecbf597dc7a7",
+    "url": "/MeditationTherapy/static/media/Group 4.aa6d1317.png"
+  },
+  {
+    "revision": "4ba73044e92ff3402b07332d572f83d2",
+    "url": "/MeditationTherapy/static/media/Group 5.4ba73044.png"
+  },
+  {
+    "revision": "9b0cc1999c77e081280c8932b3812f6d",
+    "url": "/MeditationTherapy/static/media/Group 6.9b0cc199.png"
+  },
+  {
+    "revision": "6234f3a320dc1341178ac39d45fd7470",
+    "url": "/MeditationTherapy/static/media/Group 7.6234f3a3.png"
+  },
+  {
+    "revision": "40246bb58f9a7765883470d849f4407f",
+    "url": "/MeditationTherapy/static/media/Group 8.40246bb5.png"
+  },
+  {
+    "revision": "80ff6c3642a81199b3ccf9949a0670f6",
+    "url": "/MeditationTherapy/static/media/Group 9.80ff6c36.png"
+  },
+  {
+    "revision": "7c4a775fa763d4d8ec49146291a328d8",
+    "url": "/MeditationTherapy/static/media/Meditate.7c4a775f.png"
+  },
+  {
+    "revision": "c68547da19a56eda6a458c9effe26508",
+    "url": "/MeditationTherapy/static/media/logo.c68547da.png"
+  }
+]);
